9944efe61c1ee32d7667698fd08ce7840145638f
Dharshini <dharshinipandian2005@gmail.com>
2024-10-07 20:33:45 +0530

    calendar component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainRouter from './Router/MainRouter';
+import CalendarPopup from './Components/CalendarPopup/CalendarPopup';
+import Button from '@mui/material/Button';
+import './App.css'
+import { Container } from '@mui/material';
+import Test from './Components/CalendarPopup/Calendar'
 
 const App = () => {
+  const [open, setOpen] = useState(true)
+  const handleClick = () => {
+    setOpen(!open)
+  }
   return (
-    <div>
-      <MainRouter />
-    </div>
+    // <Test />
+    <Container className='app'>
+      {/* <MainRouter /> */}
+      <Button variant="contained" onClick={handleClick}>Open</Button>
+      {open && 
+      <CalendarPopup open={open} setOpen={setOpen} /> }
+    </Container>
   );
 }
 
